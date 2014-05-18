6d2e76ce6ad6f7bcddbf98d6a0cac340bb08d3fc
gnehapk <gnehapk@gmail.com>
2014-05-18 17:52:06 +0530

    Update application.js

--- ember/js/application.js
+++ ember/js/application.js
@@ -1,4 +1,3 @@
-window.Todos = Ember.Application.create();
+window.Todos = Ember.Application.create();   //create a new instance of Ember.Application 
 
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
-
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();  // Adapters are responsible for communicating with a source of data for your application.
