21b53e54cc540cc9137fc2e8d5ab6df92a9e58f6
gnehapk <gnehapk@gmail.com>
2014-05-18 17:52:15 +0530

    Update router.js

--- ember/js/router.js
+++ ember/js/router.js
@@ -1,9 +1,9 @@
 Todos.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }); //tell Ember.js to detect when the application's URL matches '/' and to render the todos template.
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
-    return this.store.find('todo');
+    return this.store.find('todo');  //returns all the existing todos:
   }
 });
